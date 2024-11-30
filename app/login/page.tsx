67b914edb3f297e89041a535daeff7d88924cc5e
Arturbul <artur.bula2001@gmail.com>
2024-11-30 13:59:28 +0100

    login form layout update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
-import AcmeLogo from "@/app/ui/acme-logo";
-import LoginForm from "@/app/ui/login-form";
+import AcmeLogo from "@/app/ui/app-logo";
+//import LoginForm from "@/app/ui/old_login-form";
+import { LoginForm } from "../ui/login-form";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,9 +9,9 @@ export const metadata: Metadata = {
 export default function LoginPage() {
 	return (
 		<main className="flex items-center justify-center md:h-screen">
-			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-				<div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-					<div className="w-32 text-white md:w-36">
+			<div className="relative mx-auto flex w-full max-w-md flex-col space-y-2.5 p-4 md:-mt-32">
+				<div className="flex h-20 w-full items-center rounded-lg bg-stone-900 p-3 md:h-36">
+					<div className="w-full text-white md:w-full justify-center align-middle">
 						<AcmeLogo />
 					</div>
 				</div>
