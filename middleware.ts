5e8e8a04e33d9be9fb1e459a87ca233ceab28bb1
Arturbul <artur.bula2001@gmail.com>
2024-11-30 17:36:27 +0100

    intro token validation

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,58 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-	const token = req.cookies.get("auth_token");
+async function verifyToken(token: string) {
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+	try {
+		const response = await fetch(`${apiUrl}/api/auth/valid`, {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${token}`, // Token is sent in the Authorization header
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (response.ok) {
+			console.log("token verified");
+			const data = await response.json();
+			return data.valid;
+		} else {
+			throw new Error(
+				`Token verification failed with status: ${response.status}`
+			);
+		}
+	} catch (error) {
+		console.error("Token verification error:", error);
+		return false;
+	}
+}
+
+export async function middleware(req: NextRequest) {
+	const token = req.cookies.get("auth_token")?.value;
 	const url = req.url;
-	// Jeśli token nie istnieje, przekierowujemy na stronę logowania
-	if (!token && !req.url.includes("/login")) {
-		const redirectUrl = new URL("/login", url);
-		redirectUrl.searchParams.set("redirectTo", url); // Zapisujemy URL do parametru redirectTo
-		return NextResponse.redirect(redirectUrl);
+	if (!token) {
+		if (!url.includes("/login")) {
+			const redirectUrl = new URL("/login", url);
+			redirectUrl.searchParams.set("redirectTo", url);
+			return NextResponse.redirect(redirectUrl);
+		}
+		return NextResponse.next();
 	}
 
-	// Jeśli token jest obecny, możemy przejść dalej
-	return NextResponse.next();
+	const isTokenValid = await verifyToken(token);
+
+	if (isTokenValid) {
+		console.log("token valid");
+		return NextResponse.next();
+	} else {
+		console.log("Invalid token, redirecting to login...");
+		const redirectUrl = new URL("/login", url);
+		redirectUrl.searchParams.set("redirectTo", url);
+		return NextResponse.redirect(redirectUrl);
+	}
 }
 
 export const config = {
-	// https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 	matcher: ["/dashboard/:path*"],
 };
