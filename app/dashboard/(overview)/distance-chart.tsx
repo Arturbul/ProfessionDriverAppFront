d9e243d09510c7a118c9b80ac77cd6d84f3d20d9
Arturbul <artur.bula2001@gmail.com>
2024-12-17 19:43:08 +0100

    file name change

--- app/dashboard/(overview)/distance-chart.tsx
+++ app/dashboard/(overview)/distance-chart.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { getDriverDistanceByYear } from "../companies/getData";
-import DriverDistanceChart from "@/app/ui/drivers/revenue-chart";
+import DriverDistanceChart from "@/app/ui/drivers/distance-chart";
 
 export default async function DistanceChart() {
 	const distances = await getDriverDistanceByYear();
